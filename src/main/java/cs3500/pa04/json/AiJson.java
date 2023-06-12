e5ce17a5042ed6fc75293c481fca9e41a9744469
akuatbekov23 <akuatbekov23@gmail.com>
2023-06-11 20:41:50 -0400

    Styling

--- src/main/java/cs3500/pa04/json/AiJson.java
+++ src/main/java/cs3500/pa04/json/AiJson.java
@@ -1,6 +1,13 @@
 package cs3500.pa04.json;
 
-public record AiJson (
-
+/**
+ * Represents the AI in the Battle.
+ */
+public record AiJson(
+    String name,
+    String board,
+    String shotsTaken,
+    String successfulHits,
+    String reportDamage
 ) {
 }
